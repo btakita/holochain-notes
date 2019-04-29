902a39ea40a60f0017f3988122272d09e1f75ebe
Brian Takita <brian.takita@gmail.com>
2019-04-29 11:43:36 -0400

    bin/dot-output.sh
    	dot output goes into ./build dir
    
    doc_project/build/doc-libs.svg -> doc_project/build/tech.svg
    
    doc_project/doc-libs.dot -> doc_project/tech.dot

--- bin/dot-output.sh
+++ bin/dot-output.sh
@@ -3,5 +3,5 @@ FILE=$1
 DIR="$(dirname $FILE)"
 NAME__FILE="$(basename $FILE)"
 NAME__FILE__NOEXT="${NAME__FILE%.*}"
-/usr/bin/dot -Tsvg $FILE -o"$DIR/$NAME__FILE__NOEXT.svg"
-/usr/bin/dot -Tpng $FILE -o"$DIR/$NAME__FILE__NOEXT.png"
+/usr/bin/dot -Tsvg $FILE -o"$DIR/build/$NAME__FILE__NOEXT.svg"
+/usr/bin/dot -Tpng $FILE -o"$DIR/build/$NAME__FILE__NOEXT.png"
